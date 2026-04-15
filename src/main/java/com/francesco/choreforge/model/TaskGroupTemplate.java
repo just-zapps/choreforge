425027e4555b581dd7387dfb6ebaba17da53a0be
Francesco Zappalà <zappala.francesco00@gmail.com>
2026-04-15 17:51:13 +0200

    update TaskGroupTemplate so it contains multiple items

--- src/main/java/com/francesco/choreforge/model/TaskGroupTemplate.java
+++ src/main/java/com/francesco/choreforge/model/TaskGroupTemplate.java
@@ -1,13 +1,17 @@
 package com.francesco.choreforge.model;
 
+import java.util.List;
+
 public class TaskGroupTemplate {
 
     private Long id;
     private String name;
+    private List<TaskGroupItem> items;
 
-    public TaskGroupTemplate(Long id, String name) {
+    public TaskGroupTemplate(Long id, String name, List<TaskGroupItem> items) {
         this.id = id;
         this.name = name;
+        this.items = items;
     }
 
     public Long getId() {
@@ -16,5 +20,9 @@ public class TaskGroupTemplate {
 
     public String getName() {
         return name;
+    }
+
+    public List<TaskGroupItem> getItems() {
+        return items;
     }
 }
