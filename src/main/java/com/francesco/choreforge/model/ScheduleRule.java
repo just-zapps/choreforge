599324687a4e82b74c86ab326958faf65f16649c
Francesco Zappalà <zappala.francesco00@gmail.com>
2026-04-15 18:43:06 +0200

    overload ScheduleRule for single/bundle tasks

--- src/main/java/com/francesco/choreforge/model/ScheduleRule.java
+++ src/main/java/com/francesco/choreforge/model/ScheduleRule.java
@@ -6,10 +6,18 @@ public class ScheduleRule {
 
     private DayOfWeek dayOfWeek;
     private TaskGroupTemplate group;
+    private TaskTemplate taskTemplate;
 
     public ScheduleRule(DayOfWeek dayOfWeek, TaskGroupTemplate group) {
         this.dayOfWeek = dayOfWeek;
         this.group = group;
+        this.taskTemplate = null;
+    }
+
+    public ScheduleRule(DayOfWeek dayOfWeek, TaskTemplate taskTemplate) {
+        this.dayOfWeek = dayOfWeek;
+        this.group = null;
+        this.taskTemplate = taskTemplate;
     }
 
     public DayOfWeek getDayOfWeek() {
@@ -18,5 +26,17 @@ public class ScheduleRule {
 
     public TaskGroupTemplate getGroup() {
         return group;
+    }
+
+    public TaskTemplate getTaskTemplate() {
+        return taskTemplate;
+    }
+
+    public boolean isGroupRule() {
+        return group != null;
+    }
+
+    public boolean isTaskRule() {
+        return taskTemplate != null;
     }
 }
