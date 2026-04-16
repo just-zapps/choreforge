e111dcbb07062dc5d2a481d4467d54a0637787f0
Francesco Zappalà <zappala.francesco00@gmail.com>
2026-04-16 12:41:57 +0200

    separate demo data from GenerationService

---
+++ src/main/java/com/francesco/choreforge/repository/DemoDataRepository.java
@@ -0,0 +1,51 @@
+package com.francesco.choreforge.repository;
+
+import com.francesco.choreforge.model.*;
+import org.springframework.stereotype.Repository;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+@Repository
+public class DemoDataRepository {
+
+    public List<Player> getPlayers() {
+        return List.of(
+                new Player(1L, "Player1"),
+                new Player(2L, "Player2")
+        );
+    }
+
+    public List<ScheduleRule> getScheduleRules() {
+
+        TaskTemplate cleanToilet = new TaskTemplate(1L, "Clean toilet", 10, 5);
+        TaskTemplate cleanSink = new TaskTemplate(2L, "Clean sink", 8, 4);
+        TaskTemplate emptyBin = new TaskTemplate(3L, "Empty bin", 5, 2);
+        TaskTemplate tidyDesk = new TaskTemplate(4L, "Tidy desk", 6, 3);
+        TaskTemplate takeOutTrash = new TaskTemplate(5L, "Take out trash", 4, 2);
+        TaskTemplate washDishes = new TaskTemplate(6L, "Wash dishes", 5, 2);
+
+
+        List<TaskGroupItem> bathroomItems = List.of(
+                new TaskGroupItem(cleanToilet, true),
+                new TaskGroupItem(cleanSink, true),
+                new TaskGroupItem(emptyBin, true)
+        );
+
+        List<TaskGroupItem> studyItems = List.of(
+                new TaskGroupItem(tidyDesk, true)
+        );
+
+        TaskGroupTemplate bathroomGroup = new TaskGroupTemplate(1L, "Bathroom", bathroomItems);
+        TaskGroupTemplate studyGroup = new TaskGroupTemplate(2L, "Study", studyItems);
+
+
+        return List.of(
+                new ScheduleRule(DayOfWeek.TUESDAY, bathroomGroup),
+                new ScheduleRule(DayOfWeek.WEDNESDAY, studyGroup),
+                new ScheduleRule(DayOfWeek.TUESDAY, takeOutTrash),
+                new ScheduleRule(washDishes)
+        );
+    }
+
+}
--- src/main/java/com/francesco/choreforge/service/GenerationService.java
+++ src/main/java/com/francesco/choreforge/service/GenerationService.java
@@ -1,9 +1,9 @@
 package com.francesco.choreforge.service;
 
 import com.francesco.choreforge.model.*;
+import com.francesco.choreforge.repository.DemoDataRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,43 +13,18 @@ import java.util.List;
 @Service
 public class GenerationService {
 
+    private final DemoDataRepository demoDataRepository;
+
+    public GenerationService(DemoDataRepository demoDataRepository) {
+        this.demoDataRepository = demoDataRepository;
+    }
+
     public List<TaskInstance> generateWeek(LocalDate startDate) {
 
-        List<Player> players = List.of(
-                new Player(1L, "Player1"),
-                new Player(2L, "Player2")
-        );
-
-        TaskTemplate cleanToilet = new TaskTemplate(1L, "Clean toilet", 10, 5);
-        TaskTemplate cleanSink = new TaskTemplate(2L, "Clean sink", 8, 4);
-        TaskTemplate emptyBin = new TaskTemplate(3L, "Empty bin", 5, 2);
-        TaskTemplate tidyDesk = new TaskTemplate(4L, "Tidy desk", 6, 3);
-        TaskTemplate takeOutTrash = new TaskTemplate(5L, "Take out trash", 4, 2);
-        TaskTemplate washDishes = new TaskTemplate(6L, "Wash dishes", 5, 2);
-
-
-        List<TaskGroupItem> bathroomItems = List.of(
-          new TaskGroupItem(cleanToilet, true),
-          new TaskGroupItem(cleanSink, true),
-          new TaskGroupItem(emptyBin, true)
-        );
-
-        List<TaskGroupItem> studyItems = List.of(
-                new TaskGroupItem(tidyDesk, true)
-        );
-
-        TaskGroupTemplate bathroomGroup = new TaskGroupTemplate(1L, "Bathroom", bathroomItems);
-        TaskGroupTemplate studyGroup = new TaskGroupTemplate(2L, "Study", studyItems);
-
-        List<ScheduleRule> rules = List.of(
-                new ScheduleRule(DayOfWeek.TUESDAY, bathroomGroup),
-                new ScheduleRule(DayOfWeek.WEDNESDAY, studyGroup),
-                new ScheduleRule(DayOfWeek.TUESDAY, takeOutTrash),
-                new ScheduleRule(washDishes)
-        );
+        List<Player> players = demoDataRepository.getPlayers();
+        List<ScheduleRule> rules = demoDataRepository.getScheduleRules();
 
         List<TaskInstance> result = new ArrayList<>();
-
         int playerIndex = 0;
 
         for (int i = 0; i < 7; i++) {
